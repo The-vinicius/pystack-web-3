1a6960a3d570b7da32bc12fe238b63554869b0bf
The-vinicius <viniciusnao41@gmail.com>
2022-04-10 19:55:45 +0000

    ultimate

---
+++ jobs/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import encontrar_jobs, aceitar_job, perfil, enviar_projeto
+
+urlpatterns = [
+	path('encontrar_jobs/', encontrar_jobs, name="encontrar_jobs"),
+	path('aceitar_job/<int:id>/', aceitar_job, name="aceitar_job"),
+	path('perfil/', perfil, name="perfil"),
+	path('enviar_projeto/', enviar_projeto, name="enviar_projeto"),
+	]
---
+++ media/h.sh
@@ -0,0 +1,4 @@
+cd django_ecommerce;
+source bin/activate;
+cd src;
+python manage.py runserver;
